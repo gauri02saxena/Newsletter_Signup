451029c8a55e9ad1634b69cc7a7541f7234a5457
gauri02saxena <101066452+gauri02saxena@users.noreply.github.com>
2023-07-11 00:55:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,23 +37,23 @@ app.post("/", function (req, res) {
   //converting data to string
   var jsonData = JSON.stringify(data);
 
-  //Storing api key, audience id and api server
-  const MY_API_KEY = process.env.API_KEY;
+  // //Storing api key, audience id and api server
+  // const MY_API_KEY = process.env.d90c942fbfda6d3227e3f5fd22588a81-us21;
 
-  const MY_AUDIENCE_ID = process.env.AUDIENCE_ID;
+  // const MY_AUDIENCE_ID = process.env.55268a0877;
 
-  const MY_API_SERVER = process.env.API_SERVER;
+  // const MY_API_SERVER = process.env.us21;
 
   //each audience has different audience id
   const url =
-    "https://" +
-    MY_API_SERVER +
-    ".api.mailchimp.com/3.0/lists/" +
-    MY_AUDIENCE_ID;
+    "https://us21.api.mailchimp.com/3.0/lists/55268a0877" ;
+
+    
+
 
   const options = {
     method: "POST",
-    auth: "gauri24:" + MY_API_KEY,
+    auth: "gauri24:d90c942fbfda6d3227e3f5fd22588a81-us21",
   };
 
   //creating https request to mailchimp to add a subscriber on its external server
